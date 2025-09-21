5d940b94529af9ae8e71a8cd9bf6ffe6b129192d
jaytjz <jeremytanjanzhe@gmail.com>
2025-09-21 16:07:42 +0800

    finish landing ui

---
+++ frontend/src/components/Actions.jsx
@@ -0,0 +1,166 @@
+import styled from 'styled-components';
+import { Icon } from '@iconify/react/dist/iconify.js';
+
+const Actions = ({ openRegisterModal, openLoginModal }) => {
+    return(
+        <Container>
+            <div className="cta">
+                <h1>Happening Now</h1>
+                <h2>Join today.</h2>
+            </div>
+            <div className='actions'>
+                <div className="buttons">
+                    <Button>
+                        <Icon className="btn-icon" icon="ph:github-logo-fill" />
+                        Sign up with GitHub
+                    </Button>
+                    <div className='separator'>
+                        <Line />
+                            <span>or</span>
+                        <Line/>
+                    </div>
+                    <Button $primary>
+                        Create Account
+                    </Button>
+                    <div className='sign-in'>
+                        <span>Already have an account?</span>
+                        <Button $negative>
+                            Sign in
+                        </Button>
+                        <Button $negative>
+                            Try the demo account
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default Actions
+
+const Container = styled.div`
+  & .cta {
+    font-family: 'ChirpExtended';
+    font-weight: 900;
+    line-height: 13rem;
+
+    & > h1 {
+      font-size: 6rem;
+    }
+
+    & > h2 {
+      font-size: 3rem;
+    }
+  }
+
+  & .actions {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+
+    & .buttons {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+    }
+
+    & .separator {
+      display: grid;
+      grid-template-columns: 1fr auto 1fr;
+      align-items: center;
+      gap: 2rem;
+
+      align-self: stretch;
+      font-size: 1.5rem;
+    }
+
+    & .sign-in {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      & span {
+        font-size: 1.3rem;
+        font-weight: 700;
+      }
+    }
+  }
+
+  @media screen and (max-width: 1100px) {
+    & .cta {
+      & > h1 {
+        font-size: 5rem;
+      }
+    }
+  }
+  @media screen and (max-width: 900px) {
+    & .cta {
+      & > h1 {
+        font-size: 4rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    align-self: start;
+    padding: 2rem;
+
+    display: grid;
+    align-items: center;
+    grid-template-rows: auto 1fr;
+
+    & .cta {
+      line-height: 8rem;
+      padding-top: 3rem;
+      & > h1 {
+        font-size: 5rem;
+      }
+    }
+
+    & .actions {
+      align-self: flex-start;
+      margin-top: 10rem;
+      & .buttons {
+        align-self: stretch;
+      }
+
+      & .sign-in {
+        & span {
+          font-size: 1.5rem;
+        }
+      }
+    }
+  }
+`;
+export const Button = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 0;
+
+  gap: 1rem;
+
+  padding: 1rem 5rem;
+  background-color: ${(props) => (props.$primary ? 'var(--twitter-blue)' : props.$negative ? 'var(--black)' : 'var(--white)')};
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: ${(props) => (props.$primary ? 'var(--light)' : props.$negative ? 'var(--twitter-blue)' : 'var(--black)')};
+  border-radius: 3rem;
+  cursor: pointer;
+  transition: all 0.3s;
+  border: ${(props) => (props.$negative ? '1px solid var(--gray)' : null)};
+
+  & > .btn-icon {
+    font-size: 3rem;
+  }
+
+  &:hover {
+    background-color: ${(props) => (props.$primary ? 'var(--twitter-blue-hover)' : props.$negative ? 'rgba(29,155,240,0.09)' : '#eaeaea')};
+  }
+`;
+
+export const Line = styled.div`
+  height: 1px;
+  background-color: var(--gray);
+`;
--- frontend/src/views/Landing.jsx
+++ frontend/src/views/Landing.jsx
@@ -1,10 +1,63 @@
+import styled from 'styled-components';
+import TNTLogo from '../assets/ttn-logo.png';
+import { useState, useEffect } from 'react';
+
+import Actions from '../components/Actions';
+import SmallPrint from '../components/SmallPrint';
+
 const Landing = () => {
+    const [registerModalIsOpen, setRegisterModalIsOpen] = useState(false);
+    const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
+    const openRegisterModal = () => setRegisterModalIsOpen(true);
+    const openLoginModal = () => setLoginModalIsOpen(true);
+
     return (
-        <div>
-            <h1>Welcome to Fake Twitter Clone</h1>
-            <p>This is a simple landing page for the Fake Twitter Clone application.</p>
-        </div>
+        <Wrapper>
+            <div className="logo-container">
+                <img src={TNTLogo} alt="TNT Logo" />
+            </div>
+            <Actions openRegisterModal={openRegisterModal} openLoginModal={openLoginModal} />
+            <SmallPrint />
+        </Wrapper>
     )
 }
 
 export default Landing;
+
+const Wrapper = styled.main`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr auto;
+
+    align-items: center;
+
+    background-color: var(--black);
+    color: var(--light);
+
+    & .logo-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & img {
+      max-width: 30vw;
+    }
+
+    @media screen and (max-width: 800px) {
+        grid-column: span 2;
+        justify-self: start;
+        margin-left: 2rem;
+        margin-top: 2rem;
+
+        & img {
+            max-width: 15vw;
+        }
+        }
+    }
+
+    @media screen and (max-width: 800px) {
+        justify-items: center;
+        grid-template-columns: 1fr;
+        grid-template-rows: auto 1fr auto;
+    }
+`;
